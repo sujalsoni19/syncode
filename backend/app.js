4e229c74ce7a84b0a15e36d36dca7bbfc932d28c
sujalsoni19 <sujal19soni2004@gmail.com>
2026-03-25 23:51:35 +0530

    add auth and global errorHandler middleware

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
+  }),
+);
 
 app.use(express.json({ limit: "16kb" }));
 
@@ -15,12 +17,14 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(cookieParser());
 
-
-
 import userRouter from "./routes/user.route.js";
 
 //http://localhost:4000/api/v1/users/register
 app.use("/api/v1/users", userRouter);
 
+// import global error handler
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
 
-export {app};
+app.use(errorHandler);
+
+export { app };
