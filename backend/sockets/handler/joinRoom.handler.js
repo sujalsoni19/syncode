a5912f72b9b618ff196c084904007c835f861018
sujalsoni19 <sujal19soni2004@gmail.com>
2026-04-03 17:32:26 +0530

    feat: implement join room functionality with participant management

---
+++ backend/sockets/handler/joinRoom.handler.js
@@ -0,0 +1,38 @@
+import {
+  addParticipant,
+  getParticipants,
+} from "../../memory/roomParticipants.js";
+import generateGuestName from "../../utils/generateGuestName.js";
+import generateColor from "../../utils/generateColors.js";
+
+const joinRoom = (socket) => {
+  socket.on("join-room", ({ roomId, user }) => {
+    const participants = getParticipants(roomId);
+
+    const already_joined = participants.find((p) => p.socketId === socket.id);
+
+    if (already_joined) {
+      return;
+    }
+
+    socket.join(roomId);
+    console.log("user joined the room");
+
+    const participant = {
+      socketId: socket.id,
+      userId: user?._id || null,
+      name: user?.username || generateGuestName(),
+      color: generateColor(roomId),
+    };
+
+    addParticipant(roomId, participant);
+
+    socket.emit("participants", getParticipants(roomId));
+    console.log("participants: ", getParticipants(roomId));
+    socket.to(roomId).emit("user-joined", participant);
+
+    // console.log(roomId, participant);
+  });
+};
+
+export default joinRoom;
--- backend/sockets/socketManager.js
+++ backend/sockets/socketManager.js
@@ -1,8 +1,25 @@
+import joinRoom from "./handler/joinroom.handler.js";
+import codeChange from "./handler/codeChange.handler.js";
+import {
+  findParticipantRoom,
+  removeParticipant,
+} from "../memory/roomParticipants.js";
+
 const socketManager = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+    joinRoom(socket);
+    codeChange(socket);
 
     socket.on("disconnect", () => {
+      const roomId = findParticipantRoom(socket.id);
+
+      if (!roomId) return;
+
+      removeParticipant(roomId, socket.id);
+
+      socket.to(roomId).emit("user-left", socket.id);
+
       console.log("User disconnected:", socket.id);
     });
   });
