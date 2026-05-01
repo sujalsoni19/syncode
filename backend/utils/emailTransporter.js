9ff43bdd745d4dd3100c38a58a223695c6a51f34
sujalsoni19 <sujal19soni2004@gmail.com>
2026-05-01 19:33:09 +0530

    fix: making ipv4 compulsary for email delivery

--- backend/utils/emailTransporter.js
+++ backend/utils/emailTransporter.js
@@ -7,14 +7,12 @@ export const getTransporter = () => {
     transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
-      secure: false, // TLS upgrade
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS, // Gmail App Password
+        pass: process.env.SMTP_PASS,
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
+      family: 4, // force IPv4
     });
   }
 
